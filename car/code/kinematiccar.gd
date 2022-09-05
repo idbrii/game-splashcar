e25540d2d909966b3891e64371ed244018b31cb6
David Briscoe <idbrii@gmail.com>
2022-09-04 22:37:28 -0700

    Mostly functioning car but it never stops
    Matches the original project's behaviour.

--- car/code/kinematiccar.gd
+++ car/code/kinematiccar.gd
@@ -1,50 +1,79 @@
 extends KinematicBody
 
-export var gravity = -20.0
-export var wheel_base = 0.6
-export var steering_limit = 10.0
-export var engine_power = 6.0
-export var braking = -9.0
-export var friction = -2.0
-export var drag = -2.0
-export var max_speed_reverse = 3.0
+class_name KinematicCar
 
-var acceleration = Vector3.ZERO
-var velocity = Vector3.ZERO
-var steer_angle = 0.0
+export(float, 100,   0,    1) var gravity := -20.0
+export(float, 0.001, 180,  1) var steering_max_angle := 30.0 # degrees
+export(float, 0,     5,    1) var steering_exaggeration := 1.0
+export(float, 0.001, 100,  1) var engine_power := 6.0
+export(float, 0.001, 100,  1) var braking := 9.0
+export(float, -100,  0,    1) var friction := -2.0
+export(float, -100,  0,    1) var drag := -2.0
+export(float, 0.001, 1000, 1) var max_speed_reverse := 3.0
 
-func _physics_process(delta):
+export(NodePath) var nodepath_wheel_left = null
+export(NodePath) var nodepath_wheel_right = null
+onready var wheel_left := get_node(nodepath_wheel_left) as Spatial
+onready var wheel_right := get_node(nodepath_wheel_right) as Spatial
+export(NodePath) var nodepath_axle_front = null
+export(NodePath) var nodepath_axle_rear = null
+onready var axle_front := get_node(nodepath_axle_front) as Spatial
+onready var axle_rear := get_node(nodepath_axle_rear) as Spatial
+export(float) onready var wheel_base := abs(axle_front.global_translation.z - axle_rear.global_translation.z)
+
+var velocity := Vector3.ZERO
+
+
+func process_drive(dt, turn_input: float, power_input: float):
+    var acceleration := Vector3.ZERO
     if is_on_floor():
-        get_input()
-        apply_friction(delta)
-        calculate_steering(delta)
-    acceleration.y = gravity
-    velocity += acceleration * delta
-    velocity = move_and_slide_with_snap(velocity,
-                -transform.basis.y, Vector3.UP, true)
+        if power_input > 0:
+            acceleration = -transform.basis.z * engine_power
+        if power_input < 0:
+            acceleration = transform.basis.z * braking
 
-func apply_friction(delta):
+        var steer_angle := turn_input * deg2rad(steering_max_angle)
+        var wheel_turn := steer_angle * steering_exaggeration
+        wheel_right.rotation.y = wheel_turn
+        wheel_left.rotation.y = wheel_turn
+
+        var new_heading := calculate_heading(dt, velocity, steer_angle)
+
+        var d = new_heading.dot(velocity.normalized())
+        if d > 0:
+            velocity = new_heading * velocity.length()
+        elif d < 0:
+            velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+
+        look_at(transform.origin + new_heading, transform.basis.y)
+
     if velocity.length() < 0.2 and acceleration.length() == 0:
         velocity.x = 0
         velocity.z = 0
-    var friction_force = velocity * friction * delta
-    var drag_force = velocity * velocity.length() * drag * delta
-    acceleration += drag_force + friction_force
+    acceleration = apply_friction(dt, velocity, acceleration)
 
-func calculate_steering(delta):
-    var rear_wheel = transform.origin + transform.basis.z * wheel_base / 2.0
-    var front_wheel = transform.origin - transform.basis.z * wheel_base / 2.0
-    rear_wheel += velocity * delta
-    front_wheel += velocity.rotated(transform.basis.y, steer_angle) * delta
-    var new_heading = rear_wheel.direction_to(front_wheel)
+    acceleration.y = gravity
+    velocity += acceleration * dt
+    velocity = move_and_slide_with_snap(
+        velocity,
+        -transform.basis.y,
+        Vector3.UP,
+        true)
 
-    var d = new_heading.dot(velocity.normalized())
-    if d > 0:
-        velocity = new_heading * velocity.length()
-    if d < 0:
-        velocity = -new_heading * min(velocity.length(), max_speed_reverse)
-    look_at(transform.origin + new_heading, transform.basis.y)
 
-func get_input():
-    # Override this in inherited scripts for controls
-    pass
+func apply_friction(dt, vel, acc):
+    var friction_force = vel * friction * dt
+    var drag_force = vel * vel.length() * drag * dt
+    acc += drag_force + friction_force
+    return acc
+
+
+func calculate_heading(dt, vel, steer_angle) -> Vector3:
+    var front_wheel = axle_front.global_translation
+    var rear_wheel = axle_rear.global_translation
+
+    front_wheel += vel.rotated(transform.basis.y, steer_angle) * dt
+    rear_wheel += vel * dt
+
+    return rear_wheel.direction_to(front_wheel)
+
