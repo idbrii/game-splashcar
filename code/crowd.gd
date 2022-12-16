bfe7bfc9d0bb00f912dd432896feacf31fcdbbcc
David Briscoe <idbrii@gmail.com>
2022-12-16 13:45:43 -0800

    Setup destinations for pedestrians to walk to
    Instead of random points, go somewhere specific. Hopefully, this will
    make them congregate a bit. Also don't get a new destination until
    you've been going to your destination for 5 seconds.

--- code/crowd.gd
+++ code/crowd.gd
@@ -15,6 +15,8 @@ var next_ped_idx := 0
 
 var animsets = preload("res://scenes/character/people_anims.tres")
 
+var walkers := {}
+
 func point_in_radius(radius):
     return Vector3(rand_range(-radius, radius), 0, rand_range(-radius, radius))
 
@@ -22,8 +24,11 @@ func _ready():
     world_size = top_left.global_translation - bottom_right.global_translation
     city_radius = world_size.length() * 0.6
     world_centre = top_left.global_translation.linear_interpolate(bottom_right.global_translation, 0.5)
+    var count := 0
     var idx := 0
+    var now := Time.get_ticks_msec()
     for ped in get_children():
+        count += 1
         ped.nav = nav
 
         idx = (idx + 1) % animsets.people_anims.size()
@@ -31,7 +36,10 @@ func _ready():
         var anim = animsets.people_anims[idx]
         ped.init_pedestrian(anim, loves_water)
 
-    yield(get_tree().create_timer(0.1), "timeout")
+        walkers[ped] = now
+
+
+    yield(get_tree().create_timer(0.2), "timeout")
 
     for ped in get_children():
         _nav_to_random_point(ped)
@@ -47,9 +55,28 @@ func _process(dt):
     if next_ped_idx >= get_child_count():
         next_ped_idx = 0
     var ped := get_child(next_ped_idx)
-    _nav_to_random_point(ped)
+    var point : Vector3
+    var now := Time.get_ticks_msec()
+    var old := now - 5 * 1000
+    if ped in walkers and walkers[ped] < old:
+        point = _random_attraction_location()
+        var target_point = nav.get_closest_point(point)
+        walkers[ped] = now
+        ped.navigate_to_point(target_point)
+
 
 func _nav_to_random_point(ped):
-    var point = point_in_radius(city_radius)
-    var target_point = nav.get_closest_point(point + world_centre)
+    var target_point = nav.get_closest_point(_find_random_point())
     ped.navigate_to_point(target_point)
+
+
+func _find_random_point():
+    var point = point_in_radius(city_radius)
+    return point + world_centre
+
+
+func _random_attraction_location():
+    var children := $"%TheCity/Attractions".get_children()
+    var idx: int = randi() % children.size()
+    var dest = children[idx]
+    return dest.global_translation
